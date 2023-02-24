bb7b115c550218c72f46c2e79a28cc3657ea4f2e
Alexander <alxgeras@mail.ru>
2023-02-24 23:08:55 +0300

    v 1.2

--- cli.php
+++ cli.php
@@ -1,60 +1,33 @@
 <?php
 
-use GeekBrains\LevelTwo\Blog\User;
-use GeekBrains\LevelTwo\Person\{Name, Person};
-use GeekBrains\LevelTwo\Blog\Post;
-use GeekBrains\LevelTwo\Blog\Repositories\InMemoryUsersRepository;
-use GeekBrains\LevelTwo\Blog\Exceptions\UserNotFoundException;
+require_once __DIR__ . '/vendor/autoload.php';
 
-//spl_autoload_register('load');
+use GeekBrains\LevelTwo\Blog\{Post, User, Comment};
 
-include __DIR__ . "/vendor/autoload.php";
+$faker = Faker\Factory::create('ru_RU');
 
-function load($className)
-{
-//приходит GeekBrains\Person\Name
-    $file = $className . ".php"; // Person/Name.php
-    $file = str_replace("\\", "/", $file);
-    //нужно src/Person/Name.php
-    if (file_exists($file)) {
-        include $file;
-    }
+$user = new User((int)$faker->uuid(),$faker->firstName(),$faker->lastName());
+$post = new Post((int)$faker->uuid(), $user,$faker->title(),$faker->realText());
+$comment = new Comment((int)$faker->uuid(),$user,$post,$faker->realText());
+
+
+if ($argv[1]){
+    $inputData = $argv[1];
 }
-var_dump($argv);
-//$faker = Faker\Factory::create('ru_RU');
-
-//echo $faker->name() . PHP_EOL;
-//echo $faker->realText(rand(100, 200)) . PHP_EOL;
-
-$name = new Name('Peter', 'Sidorov');
-
-$user = new User(1, $name, "Admin");
-echo $user;
-
-
-$name = new Name('Peter', 'Sidorov');
-$person = new Person($name, new DateTimeImmutable());
-
-
-$post = new Post(
-    1,
-    $person,
-    'Всем привет!'
-);
-
-echo $post;
-
-$name2 = new Name('Иван', 'Таранов');
-$user2 = new User(2, $name2, "User");
-$userRepository = new InMemoryUsersRepository();
-try {
-$userRepository->save($user);
-$userRepository->save($user2);
-
-
-    echo $userRepository->get(1);
-    echo $userRepository->get(2);
-    echo $userRepository->get(3);
-} catch (UserNotFoundException | Exception $e) {
-    echo $e->getMessage();
+else{
+    $inputData = null;
 }
+
+switch ($inputData){
+
+    case 'user': echo $user;
+    break;
+    case 'post': echo $post;
+    break;
+    case 'comment': echo $comment;
+    break;
+    default:
+        echo 'Введите корректные данные';
+
+}
+
